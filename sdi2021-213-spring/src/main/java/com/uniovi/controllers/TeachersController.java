9b31f3c57674f343e2b4b97b895727cd342bacc2
uo258270 <uo258270@uniovi.es>
2021-02-18 14:00:16 +0100

    SDI-2021-213-sesión2-Ejercicio-complementario3

--- sdi2021-213-spring/src/main/java/com/uniovi/controllers/TeachersController.java
+++ sdi2021-213-spring/src/main/java/com/uniovi/controllers/TeachersController.java
@@ -1,6 +1,7 @@
 package com.uniovi.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,21 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
 import com.uniovi.entities.Teacher;
 import com.uniovi.services.TeachersService;
 
-@RestController
+@Controller
 public class TeachersController {
 
 	@Autowired //Inyectar el servicio
 	private TeachersService teachersService;
 
 	@RequestMapping("/teacher/list")
-	public String getList() {
-		return teachersService.getTeachers().toString();
+	public String getList(Model model) {
+		model.addAttribute("teacherList", teachersService.getTeachers());
+		return "teacher/list";
 	}
 
 	@RequestMapping(value = "/teacher/add", method = RequestMethod.POST)
 	public String setTeacher(@ModelAttribute Teacher teacher) {
 		teachersService.addTeacher(teacher);
-		return "Ok";
+		return "redirect:/teacher/list";
 	}
 	
 	@RequestMapping(value = "/teacher/add")
@@ -34,30 +36,29 @@ public class TeachersController {
 	}
 
 	@RequestMapping("/teacher/details/{dni}")
-	public String getDetail(@PathVariable Long dni) {
-		return teachersService.getTeacher(dni).toString();
+	public String getDetail(Model model, @PathVariable Long dni) {
+		model.addAttribute("teacher", teachersService.getTeacher(dni));
+		return "teacher/details";
 	}
 
 	@RequestMapping("/teacher/delete/{dni}")
 	public String deleteTeacher(@PathVariable Long dni) {
 		teachersService.deleteTeacher(dni);
-		return "Ok";
+		return "redirect:/teacher/list";
 	}
 
-	@RequestMapping(value = "/teacher/edit/{dni}", method = RequestMethod.POST)
-	public String setTeacher(@ModelAttribute Long dni, @ModelAttribute Teacher teacher) {
-		Teacher t = teachersService.getTeacher(dni);
-		t.setNombre(teacher.getNombre());
-		t.setApellidos(teacher.getApellidos());
-		t.setCategoria(teacher.getCategoria());
-		return "Ok";
-	}
-	
+
 	@RequestMapping(value = "/teacher/edit/{dni}")
-	public String getEdit(Model model, @PathVariable Long dni) {
-		model.addAttribute("teacher", teachersService.getTeacher(dni));
-		return "teacher/edit";
+	public String getEdit(Model model, @PathVariable Long dni, @ModelAttribute Teacher teacher) {
+		teacher.setDni(dni);
+		teachersService.addTeacher(teacher);
+		return "redirect:/teacher/details/"+dni;
 	}
+	/*
+	 * @RequestMapping(value = "/teacher/edit/{dni}") public String getEdit(Model
+	 * model, @PathVariable Long dni) { model.addAttribute("teacher",
+	 * teachersService.getTeacher(dni)); return "teacher/edit"; }
+	 */
 
 	@RequestMapping(value = "/teacher/edit", method = RequestMethod.POST)
 	public String setEdit(Model model, @ModelAttribute Teacher teacher) {
