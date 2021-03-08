ad845099f45f70fbc0a2937fe0c1cff88d82f974
uo258270 <uo258270@uniovi.es>
2021-03-08 18:34:10 +0100

    SDI-2021-213-5.6-Casos Uso VistaLogin.

--- sdi2021-213-spring/src/test/java/com/uniovi/tests/NotaneitorTests.java
+++ sdi2021-213-spring/src/test/java/com/uniovi/tests/NotaneitorTests.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import com.uniovi.tests.pageobjects.PO_HomeView;
+import com.uniovi.tests.pageobjects.PO_LoginView;
 import com.uniovi.tests.pageobjects.PO_Properties;
 import com.uniovi.tests.pageobjects.PO_RegisterView;
 import com.uniovi.tests.pageobjects.PO_View;
@@ -123,6 +124,51 @@ public class NotaneitorTests {
 	//Rellenamos el formulario.
 	PO_RegisterView.fillForm(driver, "99999990B", "Josefo", "Per", "77777",
 	"77777");
+	}
+	
+	
+	// PRN. Loguearse con exito desde el ROl de Usuario, 99999990D, 123456
+	@Test
+	public void PR07() {
+		// Vamos al formulario de logueo.
+		PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+		// Rellenamos el formulario
+		PO_LoginView.fillForm(driver, "99999990A", "123456");
+		// COmprobamos que entramos en la pagina privada de Alumno
+		PO_View.checkElement(driver, "text", "Notas del usuario");
+	}
+
+	@Test
+	public void PR08() {
+		// Vamos al formulario de logueo.
+		PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+		// Rellenamos el formulario
+		PO_LoginView.fillForm(driver, "99999993D", "123456");
+		// COmprobamos que entramos en la pagina privada de Alumno
+		PO_View.checkElement(driver, "text", "modificar");
+	}
+
+	@Test
+	public void PR09() {
+		PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+		PO_LoginView.fillForm(driver, "99999988F", "123456");
+		PO_View.checkElement(driver, "text", "Gestión de Usuarios");
+	}
+
+	@Test
+	public void PR10() {
+		PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+		PO_LoginView.fillForm(driver, "99999990A", "123456");
+		PO_View.checkElement(driver, "text", "Bienvenidos a la pagina principal");
+	}
+
+	@Test
+	public void PR11() {
+		PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+		PO_LoginView.fillForm(driver, "99999990A", "123456");
+		PO_View.checkElement(driver, "text", "Esta es una zona privada la web");
+		PO_HomeView.clickOption(driver, "logout", "text", "Identifícate");
+		PO_View.checkElement(driver, "text", "Identifícate");
 	}
 
 }
